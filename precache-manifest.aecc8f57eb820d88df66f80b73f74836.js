caa92e457402d8fe60e9f404a399a9a75f877037
JulienAssouline <julien1993@hotmail.ca>
2018-12-26 16:45:37 -0500

    Updates

---
+++ precache-manifest.aecc8f57eb820d88df66f80b73f74836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb2f4660243dac34226c141a9578de6f",
+    "url": "/react-d3-app/static/media/US_mass_shootings.cb2f4660.csv"
+  },
+  {
+    "revision": "f023742c45cbddb49e01",
+    "url": "/react-d3-app/static/js/runtime~main.f023742c.js"
+  },
+  {
+    "revision": "e3e1c2190f6738469a7f",
+    "url": "/react-d3-app/static/js/main.e3e1c219.chunk.js"
+  },
+  {
+    "revision": "6ebd7386ac240e28ab92",
+    "url": "/react-d3-app/static/js/1.6ebd7386.chunk.js"
+  },
+  {
+    "revision": "e3e1c2190f6738469a7f",
+    "url": "/react-d3-app/static/css/main.c8a66782.chunk.css"
+  },
+  {
+    "revision": "8993fbfe3fac43f56b885ba5130e856c",
+    "url": "/react-d3-app/index.html"
+  }
+];
